492383ea08f6fe4e4d3ffec8f26319c842d7a576
wakhidulkhoiri <42726508+wakhidulkhoiri@users.noreply.github.com>
2019-09-28 02:10:54 +0700

    Add files via upload

---
+++ user.php
@@ -0,0 +1,13 @@
+<?php
+$stat_sseth=true;
+$msgsseth= "[---] enjoy your life";
+$stat_cp = true;
+$msgcp = "[--] enjoy your life";
+$figlet= $ijo."
+───────────────────────────────────────────
+── ▄▀ ▄▀▄ █▀▄ ▀ █▄─█ ▄▀▀ ".$kuning." [`  ||    _    _.       
+── █─ █▀█ █─█ █ █─▀█ █─▀▌".$kuning." | L||| \/(/_|`_\|".$ijo."
+── ─▀ ▀─▀ █▀─ ▀ ▀──▀ ▀▀▀───────────────────".$t.
+  $putih."creator:".$ijo." adidoank   ".$putih."[--]  ide  :".$ijo." sungging  ".$t.
+  $putih."chanel :".$ijo." adi bordir ".$putih."[--] chanel:".$ijo." sungging  ".$t.
+  $putih."invite :".$ijo." ipzme2     ".$putih."[--] invite:".$ijo."  otr3o8   ".$t;
